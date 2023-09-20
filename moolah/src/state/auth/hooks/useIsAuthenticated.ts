a08dbb9756e266d2110a9ec2aeeb587b78f2697b
José Eduardo <joseesouza14@gmail.com>
2023-09-20 20:57:22 -0300

    criando lógica de login e criando rota privada

--- moolah/src/state/auth/hooks/useIsAuthenticated.ts
+++ moolah/src/state/auth/hooks/useIsAuthenticated.ts
@@ -1,8 +1,8 @@
 import { useSetRecoilState } from "recoil";
 import { userState } from "../userState";
-import { User } from "firebase/auth";
 import { useEffect } from "react";
 import { auth } from "../../../firebase";
+import { User } from "firebase/auth";
 
 const useIsAuthenticated = () => {
   const setLogin = useSetRecoilState<User | null>(userState);
