37ade32997e41379cc349dd53e5f1fd6e342289a
José Souza <joseesouza14@gmail.com>
2023-09-22 16:38:34 -0300

    capturando usuario do local storage

--- moolah/src/guards/AuthGuard.tsx
+++ moolah/src/guards/AuthGuard.tsx
@@ -2,8 +2,12 @@ import { Navigate } from "react-router-dom";
 import React from "react";
 import { auth } from "../firebase";
 
+
 const AuthGuard = ({ children }: any) => {
-  const user = auth.currentUser;
+  const localStorageUserString = localStorage.getItem("user");
+  const userObject = localStorageUserString && JSON.parse(localStorageUserString);
+
+  const user = userObject ? userObject : auth.currentUser;
 
   if (!user) {
     return <Navigate to="/login" replace />;
